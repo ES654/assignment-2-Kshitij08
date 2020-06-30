6517e27c3dfac6c68d3c7684a8052640e64544ce
Kshitij Gajapure <gajapure.kshitij@iitgn.ac.in>
2020-06-30 23:08:23 +0530

    Update linearRegression.py

--- linearRegression/linearRegression.py
+++ linearRegression/linearRegression.py
@@ -1,13 +1,15 @@
-
 class LinearRegression():
+    
     def __init__(self, fit_intercept=True, method='normal'):
         '''
 
         :param fit_intercept: Whether to calculate the intercept for this model. If set to False, no intercept will be used in calculations (i.e. data is expected to be centered).
         :param method:
         '''
+        self.fit_intercept = fit_intercept
+        self.method = method
+        self.theta = np.array([0])
 
-        pass
 
     def fit(self, X, y):
         '''
@@ -17,7 +19,17 @@ class LinearRegression():
         :return:
         '''
 
-        pass
+        X = np.array(X)
+        y = np.array(y)
+        
+        if self.fit_intercept == True:
+            m, _ = X.shape
+            ones = np.ones((m,1))
+            X = np.concatenate((ones,X),1)
+        
+        #note to self: pinv retuns inverse of matrix when it's available, and pseuo-inverse when it's not
+        #https://hadrienj.github.io/posts/Deep-Learning-Book-Series-2.9-The-Moore-Penrose-Pseudoinverse/
+        self.theta = np.dot(np.linalg.pinv(np.dot(X.T,X)),np.dot(X.T,y))
 
     def predict(self, X):
         '''
@@ -25,8 +37,14 @@ class LinearRegression():
         :param X: pd.DataFrame with rows as samples and columns as features
         :return: y: pd.Series with rows corresponding to output variable. The output variable in a row is the prediction for sample in corresponding row in X.
         '''
-
-        pass
+        X = np.array(X)
+        
+        if self.fit_intercept == True:
+            m, _ = X.shape
+            ones = np.ones((m,1))
+            X = np.concatenate((ones,X), 1)
+        
+        return np.dot(X,self.theta)
 
     def plot_residuals(self):
         """
