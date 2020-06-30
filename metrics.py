79c829baff59d78efb60b25749e03172f13b6b6c
Kshitij Gajapure <gajapure.kshitij@iitgn.ac.in>
2020-06-30 23:05:53 +0530

    Update metrics.py

--- metrics.py
+++ metrics.py
@@ -1,8 +1,7 @@
-
 def accuracy(y_hat, y):
     """
     Function to calculate the accuracy
-
+    
     Inputs:
     > y_hat: pd.Series of predictions
     > y: pd.Series of ground truth
@@ -15,13 +14,23 @@ def accuracy(y_hat, y):
     ensure that the function does not fail in corner cases.
     """
     assert(y_hat.size == y.size)
+    
     # TODO: Write here
-    pass
+    
+    count = 0
+    
+    for i in range(len(y)):
+        if y_hat[i]==y[i]:
+            count = count+1
+            
+    accuracy = count/len(y) 
+    
+    return(accuracy)
+
 
 def precision(y_hat, y, cls):
     """
     Function to calculate the precision
-
     Inputs:
     > y_hat: pd.Series of predictions
     > y: pd.Series of ground truth
@@ -29,12 +38,34 @@ def precision(y_hat, y, cls):
     Output:
     > Returns the precision as float
     """
-    pass
+    assert(y_hat.size == y.size)
+    
+    #out of the number of times we predict Good, how many times is the condition actually Good
+    act_corr_pred = 0
+    corr_pred = 0
+    temp = 10**(-6)
+    
+    for i in range(len(y)):
+        if y_hat[i] == cls:
+            if y_hat[i] == y[i]:
+                act_corr_pred = act_corr_pred + 1
+            corr_pred = corr_pred + 1
+    
+    
+    if corr_pred>temp:
+        corr_pred = corr_pred*1
+    else:
+        corr_pred = temp
+        
+    precision = act_corr_pred/corr_pred
+    
+    return(precision)
+
+
 
 def recall(y_hat, y, cls):
     """
     Function to calculate the recall
-
     Inputs:
     > y_hat: pd.Series of predictions
     > y: pd.Series of ground truth
@@ -42,29 +73,66 @@ def recall(y_hat, y, cls):
     Output:
     > Returns the recall as float
     """
-    pass
+    assert(y_hat.size == y.size)
+        
+    #the fraction of the total amount of relevant instances that were actually retrieved
+    act_corr_pred = 0
+    corr_pred = 0
+    temp = 10**(-6)
+
+    for i in range(len(y)):
+        if y[i] == cls:
+            if y[i] == y_hat[i]:
+                act_corr_pred = act_corr_pred + 1
+            corr_pred = corr_pred + 1
+    
+    if corr_pred>r:
+        corr_pred = corr_pred*1
+    else:
+        corr_pred = temp
+        
+    recall = act_corr_pred/corr_pred
+    
+    return(recall)
+
 
 def rmse(y_hat, y):
     """
     Function to calculate the root-mean-squared-error(rmse)
-
     Inputs:
     > y_hat: pd.Series of predictions
     > y: pd.Series of ground truth
     Output:
     > Returns the rmse as float
     """
+    assert(y_hat.size == y.size)
+    
+    sum_ = 0
+    
+    for i in range(len(y)):
+        sum_ = (y_hat[i] - y[i])**2 + sum_
+        
+    rmse = (sum_/len(y))**0.5
+    
+    return(rmse)
 
-    pass
-
+    
 def mae(y_hat, y):
     """
     Function to calculate the mean-absolute-error(mae)
-
     Inputs:
     > y_hat: pd.Series of predictions
     > y: pd.Series of ground truth
     Output:
     > Returns the mae as float
     """
-    pass
+    assert(y_hat.size == y.size)
+    
+    sum_ = 0
+    
+    for i in range(len(y)):
+        sum_ = abs(y_hat[i] - y[i]) + sum_
+        
+    mae = (sum_/len(y))
+    
+    return(mae)
